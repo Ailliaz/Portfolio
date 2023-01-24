814be2c4059cab53cefde961620532416f333eb5
unknown <liadesben@gmail.com>
2023-01-24 17:02:32 -0300

    init GlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./components/styles/Globals.styled";
+
 function App() {
-  return <div></div>;
+  const theme = {
+    colors: {
+      white: "rgba(240, 247, 255, 1)",
+      lightNavyBlue: "rgba(4, 22, 48, 1)",
+      green: "rgba(0, 206, 158, 1)",
+      mint: "rgba(0, 255, 213, 1)",
+      pink: "rgba(210, 0, 233, 1)",
+      darkPurple: "rgba(19, 1, 30, 1)",
+    },
+    mobile: "768px",
+    transition: "all 650ms ease-in-out",
+  };
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        App
+        <hr />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/styles/Globals.styled.jsx
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    
+*,
+*::before,
+*::after{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+html{
+    font-family: "Share Tech Mono", monospace;
+    scroll-behavior: smooth;
+    color: ${({ theme }) => theme.colors.white};
+    background: ${({ theme }) => theme.colors.darkPurple};
+}
+
+a{
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.green};
+    transition: all 250ms ease-in-out;
+
+    &:hover{
+        color: ${({ theme }) => theme.colors.mint};
+    }
+
+}
+
+ul{
+    list-style: none;
+}
+
+hr{
+    background: linear-gradient(
+        to right,
+        ${({ theme }) => theme.colors.mint} 0%,
+        ${({ theme }) => theme.colors.pink} 100%
+    );
+    border: 0;
+    height: 1px;
+}
+
+`;
